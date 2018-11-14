43d1e84ad3eaef6ba0beded9dd38383224bd79cc
n3tsky <n3tsky@protonmail.com>
2018-11-14 22:30:04 +0100

    final push?

--- README.md
+++ README.md
@@ -3,6 +3,28 @@
 A really simple shodan parser for any given IP address (or file).
 Nothing new, probably a ton of other scripts doing the same job.
 
+Do not forget to add the "API_KEY" in the script 
+
+* For a single host
+```
+python shodan-query.py -i X.X.X.X 
+```
+
+* For a list of hosts
+```
+python shodan-query.py -f ips.txt
+```
+
+* For a list of hosts (+ wait X seconds between each request)
+```
+python shodan-query.py -f ips.txt -t X
+```
+
+* Get more info (verbosity) 
+```
+python shodan-query.py -f ips.txt -v
+```
+
 ## Requirements
 
 - shodan
--- shodan-query.py
+++ shodan-query.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import signal
+from time import sleep
 try:
     from shodan import Shodan, APIError
     import argparse
@@ -9,8 +10,14 @@ except:
     print "[!] Please install required modules (shodan, argparse)"
     sys.exit(1)
 
-# API Key goes here
+############# API Key goes here #############
 API_KEY = ""
+#############################################
+
+# GLOBAL
+BLUE = "\033[1;34m"
+RESET = "\033[0;0m"
+VERBOSE = 0
 
 # Handle SIGINT
 def exit_signal(signal, frame):
@@ -19,13 +26,11 @@ def exit_signal(signal, frame):
 
 signal.signal(signal.SIGINT, exit_signal)
 
-#
-# Basic check for API key
+# Basic check for API key (todo: valid API key?)
 def check_api():
     if len(API_KEY) == 0:
         print "[!] Please provide a Shodan API key"
         sys.exit(1)
-    # Try validity of API KEY?
 
 # Parse provided (user) args 
 def parse_args():
@@ -33,6 +38,7 @@ def parse_args():
     parser.add_argument("-i", "--ip", help="provide a single IP address")
     parser.add_argument("-f", "--file", help="provide a file with IP address(es)")
     parser.add_argument("-t", "--time", help="time to wait between requests (default: 0)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="get more information (vulns) about host")
     args = parser.parse_args()
     return parser, args
 
@@ -43,10 +49,12 @@ def read_file_into_list(filename):
     print "[!] Loaded %s IPs from \"%s\"" % (len(lines), filename)
     return lines
 
-
+# Display messages
 def display(msg, value):
     try:
-        print "%s: %s" % (msg, value)
+        msg = "%s%s%s: %s" % (BLUE, msg, RESET, value)
+        print msg
+        #print "%s: %s" % (msg, value)
     except KeyError:
         pass
 
@@ -66,9 +74,11 @@ def deliver_services(json):
         display("Ports", (", ".join(repr(str(n)) for n in json["ports"])))
 
 def deliver_http(http):
-    pass
+    display(" - HTTP","")
+    for values in ["host", "title", "robots", "server", "location", "sitemap"]:
+        display("  . %s" % values, http[values])
 
-def deliver_data(json, verbose=False):
+def deliver_data(json):
     print "\n---- Services ----"
     if "data" in json.keys():
         for d in json["data"]:
@@ -82,10 +92,11 @@ def deliver_data(json, verbose=False):
                     if (type(d[values]) == unicode) and (d[values] != None):
                         display(" - %s" % (values), d[values])
             
-            if "http" in d.keys():
+            if "http" in d.keys() and VERBOSE:
                 deliver_http(d["http"])
+            print ""
 
-def deliver_vulns(json, verbose=False):
+def deliver_vulns(json):
     print "\n---- Vulns ----"
     if "vulns" in json.keys():
         display("Vulns", (", ".join(json["vulns"])))
@@ -104,18 +115,16 @@ def deliver_vulns(json, verbose=False):
                     if "references" in v:
                         print "  References:\n  - %s" % ("\n  - ".join(v["references"]))
 
-
 def parse_response(json_response):
     j = json.loads(json.dumps(json_response))
-    print j
     print "###########################"
-    print "Host: %s" % (j["ip_str"])
+    display("Host", j["ip_str"])
     deliver_generic_info(j)
     deliver_services(j)
-    deliver_data(j, verbose=True)
+    deliver_data(j)
     print "###########################"
 
-# Query shodan for info about IP address
+# Query shodan for info about host
 def query(ip):
     try:
         host = api.host(ip)
@@ -126,19 +135,6 @@ def query(ip):
         else:
             print "[!] Unkown error (\"%s\")" % (e.value)
 
-def do(args):
-    try:
-        if (args.ip):
-            query(args.ip)
-        elif (args.file):
-            pass
-        else:
-            args.display_help()
-            print "[*] Nothing to do"
-    except APIError, e:
-        print "[!] APIError (msg: \"%s\")" % (e)
-        sys.exit(1)
-
 #####################
 # Main starts here
 #####################
@@ -148,12 +144,19 @@ if __name__ == "__main__":
     parser, args = parse_args()
     api = Shodan(API_KEY)
 
+    VERBOSE = args.verbose
+    if args.time:
+        time = int(args.time) if args.time.isdigit() else 0
+
     # Do main work
     try:
         if (args.ip):
             query(args.ip)
         elif (args.file):
-            pass
+            print "[!] Waiting %d seconds between each query" % (time)
+            for e in read_file_into_list(args.file):
+                query(e)
+                sleep(time)
         else:
             parser.print_help()
     except APIError, e:
